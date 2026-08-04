8f03654d86a98a873942b31bfe73a596eef842e8
Tanmoy Dab <_github_>
2026-08-04 16:26:42 +0530

    back change

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -124,7 +124,11 @@ export const HomeScreen: React.FC<HomeScreenProps> = ({ vm }) => {
 
         </div>
 
-      
+      </motion.div>
+
+      {/* Upload Area */}
+
+      <UploadDropZone onFileSelect={vm.uploadPhoto} />
 
       {/* ================= AI TOOLS ================= */}
 
